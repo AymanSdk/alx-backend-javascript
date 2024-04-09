4917501571121d19faeb1b7ff3fcb21eb9815c7a
AymanSdk <ayman.sdk123@oulook.com>
2024-04-09 00:02:40 +0000

    Add appendToEachArrayValue function in 0x00-ES6_basic module

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,1 +1,8 @@
-#!/usr/bin/node
+export default function appendToEachArrayValue(array, appendString) {
+  const novelArray = [];
+  for (const value of array) {
+    novelArray.push(appendString + value);
+  }
+
+  return novelArray;
+}
