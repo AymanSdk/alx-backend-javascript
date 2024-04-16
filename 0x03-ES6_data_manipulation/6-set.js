067026ff736b7f2b5faa6ce04852190d5568b8e9
AymanSdk <ayman.sdk123@oulook.com>
2024-04-16 19:32:41 +0100

    Add setFromArray function to create a set from an array

--- 0x03-ES6_data_manipulation/6-set.js
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -1,1 +1,8 @@
-#!/usr/bin/node
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
